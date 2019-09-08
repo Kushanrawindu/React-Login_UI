79b2041ea15f713ed0ac8ebd63059f5ca0e53208
Kushanrawindu <kushanrawindu@gmail.com>
2019-09-08 12:53:24 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Login} from "./component/login/index";
+import {Register} from "./component/login/register";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      isLoggingActive:true,
+    }
+  }
+
+  changeState(){
+    const {isLoggingActive} = this.state;
+    //if(isLoggingActive)
+  }
+
+  render() {
+    const { isLoggingActive } = this.state;
+    const current = isLoggingActive ? "Register" : "Login";
+    const currentActive = isLoggingActive ? "Login" : "Register";
+    return(
+        <div className="App">
+          <div className="login">
+            <div className="container">
+              {isLoggingActive && <Login containerRef={(ref) => this.current=ref}/> }
+              {!isLoggingActive && <Register containerRef={(ref) => this.current=ref}/>}
+            </div>
+            <RightSide current={current} containerRef={ref=>this.rightSide=ref} onClick={this.changeState.bind(this)}/>
+          </div>
+        </div>
+    )
+  }
+}
+
+const RightSide = props =>{
+  return <div className="right-side" ref={props.containerRef} onClick={props.onclick}>
+  <div className="inner-container">
+    <div className="text">{props.current}</div>
+  </div>
+  </div>
 }
 
 export default App;
